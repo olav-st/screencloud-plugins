74823ec25619d616fb3a69dce1d54d001ca0f0d4
olav-st <olav.s.th@gmail.com>
2015-12-19 22:20:20 +0100

    update version test script

--- .travis-version-test.sh
+++ .travis-version-test.sh
@@ -1,11 +1,15 @@
+xmllint --version >/dev/null 2>&1 || { echo "This script requires xmllint!"; exit 1; }
+
 SHORTNAME=$1
 if [ -z "$SHORTNAME" ]; then
 	echo "No plugin shortname provided. Test failed!"
 	exit 1
 fi
 echo "Testing version num of '$SHORTNAME':"
+
 VERSION=$(xmllint --xpath '//metadata/version/text()' $SHORTNAME/metadata.xml)
-LIST_VERSION=$(xmllint --xpath "plugins//plugin[shortname='$SHORTNAME']/version/text()" ./plugin-list.xml)
+LIST_VERSION=$(xmllint --xpath "//plugins//plugin[shortname='$SHORTNAME']/version/text()" ./plugin-list.xml)
+
 if [ "$VERSION" != "$LIST_VERSION" ]; then
 	echo "$VERSION != $LIST_VERSION"
 	echo "Test FAILED!"
